2bcce755b4da66214dab49fdc1e5b7079dc332a4
frankwang <chacha1351513435@gmail.com>
2020-12-14 11:47:22 +0800

    基本完成Types和NumberPad组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,12 +14,12 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
 };
 </script>
+
 <style lang="scss">
 .layout-content {
   display: flex;
