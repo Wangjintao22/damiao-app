a7cbae275cc51636345c94d53c6ca0a2e9eaab87
frankwang <chacha1351513435@gmail.com>
2020-12-18 15:17:41 +0800

    将create封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -59,14 +59,12 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record)
   }
 
   @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 
 }
