9c4116703a0f6225a8e8743f2616af83906c727b
frankwang <chacha1351513435@gmail.com>
2020-12-21 15:33:54 +0800

    将models 合并到 store里

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,7 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag.id"
+      <li v-for="tag in tagList" :key="tag.id"
           :class="{selected:selectedTags.indexOf(tag)>=0}"
           @click="toggle(tag)">{{ tag.name }}
       </li>
@@ -14,11 +14,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component
 export default class Tags extends Vue {
-  @Prop() readonly dataSource: string[] | undefined;
+  tagList = store.fetchTags();
   selectedTags: string[] = [];
 
   toggle(tag: string) {
@@ -28,19 +29,15 @@ export default class Tags extends Vue {
     } else {
       this.selectedTags.push(tag);
     }
-    this.$emit('update:value',this.selectedTags)
-  }
-  create(){
-    const name = window.prompt('请输入标签名');
-    if(name ===''){
-      window.alert('标签名不能为空');
-    } else if (this.dataSource){
-      this.$emit('update:dataSource',
-      [...this.dataSource,name]);
-    }
+    this.$emit('update:value', this.selectedTags);
   }
 
-}
+  create() {
+    const name = window.prompt('请输入标签名');
+    if (!name) {return window.alert('标签名不能为空');}
+    store.createTag(name);
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
